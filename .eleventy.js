aacba494545fc7bddd5028ea0932ed115c591beb
fenu <a.fenu3@studenti.unipi.it>
2025-10-14 22:38:14 +0200

    Adding things

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
+
 module.exports = function(eleventyConfig) {
-  // Tell Eleventy to copy the 'static' folder to the output folder
   eleventyConfig.addPassthroughCopy("static");
 
   return {
+    pathPrefix: "/loopspacecafe/", // Add this line!
+
     dir: {
-      input: "pages", // Source folder
-      includes: "../_includes", // Includes folder
-      output: "_site" // Output folder (where the final site is built)
+      input: "pages",
+      includes: "../_includes",
+      output: "_site"
     }
   };
 };
