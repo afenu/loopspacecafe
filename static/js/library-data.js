7eaf12da63fefce1b45a2fe99addea73b03466e1
fenu <a.fenu3@studenti.unipi.it>
2025-10-17 13:41:48 +0200

    Aggiunto libro di schapira

--- static/js/library-data.js
+++ static/js/library-data.js
@@ -5,27 +5,34 @@ const libraryData = [
     "title": "A Primer on Mapping Class Groups",
     "author": "Benson Farb and Dan Margalit",
     "year": 2011,
-    "url": "../static/libri/primeronmappingclassgroup.pdf" // Correct relative path
+    "url": "/static/libri/primeronmappingclassgroup.pdf"
   },
   {
     "id": 2,
     "title": "Configuration Spaces are not Homotopy Invariant",
     "author": "Riccardo Longoni and Paolo Salvatore",
     "year": 2005,
-    "url": "../static/libri/counterexampleslongonisalvatore.pdf" // Correct relative path
+    "url": "/static/libri/counterexampleslongonisalvatore.pdf"
   },
   {
     "id": 3,
     "title": "Braid Groups",
     "author": "C. Kassel and V. Turaev",
     "year": 2008,
-    "url": "../static/libri/braidgroupskasselturaev.pdf" // Correct relative path
+    "url": "/static/libri/braidgroupskasselturaev.pdf"
   },
   {
     "id": 4,
     "title": "Structure des Espaces de Configurations",
     "author": "Najib Idrissi",
     "year": 2018,
-    "url": "../static/libri/najibidrissihabilitation.pdf" // Correct relative path
+    "url": "/static/libri/najibidrissihabilitation.pdf"
+  },
+  {
+    "id": 5,
+    "title": "Algebra and Topology",
+    "author": "Pierre Schapira",
+    "year": 2021,
+    "url": "/static/libri/algtopschapira.pdf"
   }
 ];
